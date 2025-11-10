c6fa84b30e4dd15630bcfcdc679057da190138cf
musab.karatas <musab.karatas@etu.unistra.fr>
2025-11-10 14:02:23 +0100

    Update portfolio build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// Remplace 'musab01k.github.io' par le nom exact de ton repo si ce n'est pas la racine
+
 export default defineConfig({
-  base: '/musab01k.github.io/',  // <-- Ajoute cette ligne
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
