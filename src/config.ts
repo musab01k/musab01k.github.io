578fcf6cb63ae9cef7e2217cd0bcd5ae9e722950
musab.karatas <musab.karatas@etu.unistra.fr>
2025-11-12 19:49:04 +0100

    maj du portfolio projets

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Musab Karatas",
-  title: "Etudiant en dernière année de Master Informatique IA et Data Science",
+  title: "Etudiant en dernière année de Master Informatique axé sur l'IA et la Data Science",
   description: "Mon portfolio",
   accentColor: "#1d4ed8",
   social: {
@@ -13,7 +13,7 @@ export const siteConfig = {
   skills: ["Python", "C", "C++", "Java", "C#", "R", "SQL", "NoSQL", "PostgreSQL", "API REST", "Javascript"],
   projects: [
     {
-      name: "Agent conversationnel LLM intégré à une carte interactive (AgentMap",
+      name: "Agent conversationnel LLM intégré à une carte interactive (AgentMap)",
       description: `Traitement de données géospatiales (Pandas) et classification automatique des requêtes.
 — Conversion des demandes en SQL via LLM et interrogation de la base par API.
 — Visualisation des résultats sur des cartes interactives.`,
@@ -21,16 +21,19 @@ export const siteConfig = {
       skills: ["Python", "Dash", "PostgreSQL", "API REST"],
     },
     {
-      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+      name: "Classification du cancer du sein à partir de scans médicaux ",
       description:
-        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+        `onception d’un réseau de neurones (CNN) pour analyser les images et déterminer la présence de cancer.
+⇒ Modèle permettant de trier les scans pour mettre en avant les cas à risque et alléger la charge aux médecins.`,
       link: "https://fullstackextensions.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
+      skills: ["Python", "CNN", "TensorFlow", "Keras"],
     },
     {
-      name: "ExtensionKit",
+      name: "Cand Auto (projet personnel)",
       description:
-        "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
+        `Conception d’un script d’envoi automatique de candidatures, intégrant l’API
+OpenAI pour la génération dynamique de mails et lettres de motivation personnalisés, envoyés automatiquement vers
+un dataset d’adresses e-mail collectées.`,
       link: "https://extensionkit.io/?ref=devportfolio",
       skills: ["React", "Node.js", "AWS"],
     },
@@ -54,9 +57,6 @@ export const siteConfig = {
       degree: "Master Sciences de données et systèmes complexes",
       dateRange: "2024 - 2026",
       achievements: [
-        "Plus de 500 heures de cours TP et TD.",
-        "Beaucoup de projets",
-        "Spécialisation en IA, Data Science, Machine Learning, Deep Learning, etc.",
       ],
     },
     {
@@ -64,9 +64,6 @@ export const siteConfig = {
       degree: "Licence d'informatique",
       dateRange: "2021 - 2024",
       achievements: [
-        "Graduated Magna Cum Laude with 3.8 GPA",
-        "Dean's List all semesters",
-        "President of Computer Science Club",
       ],
     },
   ],
